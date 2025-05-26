e6ca56aae8324ad8087b0329c66e1d3bd20afcde
Fejanos <fenyvesijani97@gmail.com>
2025-05-26 10:48:36 +0200

    Teacher class

--- src/Model/Student.java
+++ src/Model/Student.java
@@ -4,10 +4,41 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author János
  */
 public class Student {
+    private int id;
+    private String name;
+    private Date born;
+    //private int teacher_id;
+    private Teacher teacher;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Date getBorn() {
+        return this.born;
+    }
+
+    public Teacher getTeacher() {
+        return this.teacher;
+    }
+
+    public Student(int id, String name, Date born, Teacher teacher) {
+        this.id = id;
+        this.name = name;
+        this.born = born;
+        this.teacher = teacher;
+    }
+    
     
 }
